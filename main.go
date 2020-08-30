d3e972fae672bc3f03eac8ee66b073d92963d217
Rob Murgai <smurgai@gmail.com>
2020-08-29 23:47:28 -0400

    Add exercises in Go_Lang on maps
    Signed-off-by: Rob Murgai <smurgai@gmail.com>

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ import (
 func main() {
 	fmt.Printf("######### START ###########\n\n")
 
-	capacity()
+	textArgument := "As far as eye could reach he saw nothing but the stems of the great plants about him receding in the violet shade, and far overhead the multiple transparency of huge leaves filtering the sunshine to the solemn splendour of twilight in which he walked. Whenever he felt able he ran again; the ground continued soft and springy, covered with the same resilient weed which was the first thing his hands had touched in Malacandra. Once or twice a small red creature scuttled across his path, but otherwise there seemed to be no life stirring in the wood; nothing to fear—except the fact of wandering unprovisioned and alone in a forest of unknown vegetation thousands or millions of miles beyond the reach or knowledge of man."
+	words(textArgument)
 
 	fmt.Printf("\n\n########## END ############\n")
 }
