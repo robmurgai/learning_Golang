107ad2a95dda1a1bafd07760a58056e3c1354908
Rob Murgai <smurgai@gmail.com>
2020-09-11 14:53:45 -0400

    Add Exercises on slice from Chapter 20
    Signed-off-by: Rob Murgai <smurgai@gmail.com>

--- main.go
+++ main.go
@@ -8,11 +8,36 @@ import (
 	"unicode/utf8"
 )
 
+// main is used to call all the expercise functions.
+// Words.go
+// textArgument := "As far as eye could reach he saw nothing but the stems of the great plants about him receding in the violet shade, and far overhead
+// the multiple transparency of huge leaves filtering the sunshine to the solemn splendour of twilight in which he walked. Whenever he felt able he
+// ran again; the ground continued soft and springy, covered with the same resilient weed which was the first thing his hands had touched in Malacandra.
+// Once or twice a small red creature scuttled across his path, but otherwise there seemed to be no life stirring in the wood; nothing to fear—except
+// the fact of wandering unprovisioned and alone in a forest of unknown vegetation thousands or millions of miles beyond the reach or knowledge of man."
+// words(textArgument)
 func main() {
 	fmt.Printf("######### START ###########\n\n")
 
-	textArgument := "As far as eye could reach he saw nothing but the stems of the great plants about him receding in the violet shade, and far overhead the multiple transparency of huge leaves filtering the sunshine to the solemn splendour of twilight in which he walked. Whenever he felt able he ran again; the ground continued soft and springy, covered with the same resilient weed which was the first thing his hands had touched in Malacandra. Once or twice a small red creature scuttled across his path, but otherwise there seemed to be no life stirring in the wood; nothing to fear—except the fact of wandering unprovisioned and alone in a forest of unknown vegetation thousands or millions of miles beyond the reach or knowledge of man."
-	words(textArgument)
+	//create a NewUniverse and Show it.
+	newUniverse := NewUniverse()
+	tempUniverse := NewUniverse()
+
+	//newUniverse.Show()
+
+	//Seed the Universe and Show it.
+	newUniverse.Seed()
+	newUniverse.Show()
+
+	//Clear the Screen on MAC
+	//fmt.Println("\033[H")
+
+	//Step the Universe and Show it.
+	Step(newUniverse, tempUniverse)
+	newUniverse = tempUniverse
+	//fmt.Printf("\033[H")
+
+	newUniverse.Show()
 
 	fmt.Printf("\n\n########## END ############\n")
 }
