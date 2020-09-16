2c8b7e1d19f5320f0a9eda22320631d442f770a2
Rob Murgai <smurgai@gmail.com>
2020-09-16 14:56:20 -0400

    Add methods that provide behavior to structured data
    Lesson 22 from "Get Programming with Go"
    
    Signed-off-by: Rob Murgai <smurgai@gmail.com>

--- landing.go
+++ landing.go
@@ -1,48 +1,71 @@
 package main
 
-/**
-** Write a program that displays the JSON encoding of the three rover landing sites in listing 21.8. The JSON should include the name of each landing
-** site and use struct tags as shown in listing 21.10.
-** To make the output friendlier, make use of the MarshalIndent function from the json package.
-**/
-
 import (
-	"encoding/json"
 	"fmt"
-	"os"
+	"strconv"
 )
 
-func landing() {
-	type location struct {
-		Name string  `json:"name"`
-		Lat  float64 `json:"latitude"`
-		Long float64 `json:"longitude"`
-	}
-
-	locations := []location{
-		{Name: "Bradbury Landing", Lat: -4.5895, Long: 137.4417},
-		{Name: "Columbia Memorial Station", Lat: -14.5684, Long: 175.472636},
-		{Name: "Challenger Memorial Station", Lat: -1.9462, Long: 354.4734},
-	}
-
-	bytes, err := json.Marshal(locations)
-	exitOnError(err)
-
-	fmt.Println(string(bytes))
-
-	bytes, err = json.MarshalIndent(locations, "", " ")
-	exitOnError(err)
-
-	fmt.Println()
-	fmt.Println()
-	fmt.Println(string(bytes))
-
+// coordinate in degrees, minutes, seconds in a N/S/E/W hemisphere.
+type coordinate struct {
+	d, m, s float64
+	h       rune
 }
 
-// exitOnError prints any errors and exits.
-func exitOnError(err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+// location in Name of the place, Latitude and Longitude both as coordinates
+type location struct {
+	Name string     `json:"name"`
+	Lat  coordinate `json:"latitude"`
+	Long coordinate `json:"longitude"`
+}
+
+// landing declares a location for each roq in the following table and prints out each of the locations in decimal degrees.
+//
+// Rover or lander			Landing site				Latitude			Longitude
+// Spirit					Columbia Memorial Station	14°34’6.2” S		175°28’21.5” E
+// Opportunity				Challenger Memorial Station	1°56’46.3” S		354°28’24.2” E
+// Curiosity				Bradbury Landing			4°35’22.2” S		137°26’30.1” E
+// InSight					Elysium Planitia			4°30’0.0” N			135°54’0” E
+func landing() {
+
+	// Mars the World
+	var mars = world{radius: 3389.5} //Mars
+
+	// Spirit Columbia Memorial Station	14°34’6.2” S	175°28’21.5”
+	Spirit := location{Name: "Columbia Memorial Station", Lat: coordinate{14, 34, 6.2, 'S'}, Long: coordinate{175, 28, 21.5, 'E'}}
+	fmt.Printf("Spirit landed at %v\n", Spirit.locationInDecimal())
+
+	// Opportunity	Challenger Memorial Station	1°56’46.3” S	354°28’24.2” E
+	Oppertunity := location{Name: "Challenger Memorial Station", Lat: coordinate{1, 56, 46.3, 'S'}, Long: coordinate{354, 28, 24.2, 'E'}}
+	fmt.Printf("Oppertunity landed at %v\n", Oppertunity.locationInDecimal())
+
+	// Curiosity	Bradbury Landing	4°35’22.2” S	137°26’30.1” E
+	Curiosity := location{Name: "Bradbury Landing", Lat: coordinate{4, 35, 22.2, 'S'}, Long: coordinate{137, 26, 30.1, 'E'}}
+	fmt.Printf("Curiosity landed at %v\n", Curiosity.locationInDecimal())
+
+	// InSight	Elysium Planitia	4°30’0.0” N	135°54’0” E
+	InSight := location{Name: "Elysium Planitia", Lat: coordinate{4, 30, 0.0, 'N'}, Long: coordinate{135, 54, 0, 'E'}}
+	fmt.Printf("InSight landed at %v\n", InSight.locationInDecimal())
+
+	fourMarsSites := []location{Spirit, Oppertunity, Curiosity, InSight}
+
+	for i, foo := range fourMarsSites {
+
+		for _, bar := range fourMarsSites[i+1:] {
+			fmt.Printf("Distance between %v and %v is: %.2f\n", foo.Name, bar.Name, mars.distance(foo, bar))
+		}
 	}
+}
+
+// decimal converts a d/m/s coordinate to decimal degrees.
+func (c coordinate) decimal() float64 {
+	sign := 1.0
+	switch c.h {
+	case 'S', 'W', 's', 'w':
+		sign = -1
+	}
+	return sign * (c.d + c.m/60 + c.s/3600)
+}
+
+func (l location) locationInDecimal() string {
+	return l.Name + " and " + strconv.FormatFloat(l.Lat.decimal(), 'f', -1, 64) + ", " + strconv.FormatFloat(l.Long.decimal(), 'f', -1, 64)
 }
--- main.go
+++ main.go
@@ -8,31 +8,21 @@ import (
 func main() {
 	fmt.Printf("######### START ###########\n\n")
 
-	landing()
+	//landing()
+
+	var earth = world{radius: 6371.0} //Earth
+
+	//Find the distance from London, England (51°30’N 0°08’W) to Paris, France (48°51’N 2°21’E).
+	london := location{Name: "London, England", Lat: coordinate{51, 30, 0, 'N'}, Long: coordinate{0, 8, 0, 'W'}}
+	paris := location{Name: "Paris, France", Lat: coordinate{48, 51, 0, 'N'}, Long: coordinate{2, 21, 0, 'E'}}
+	fmt.Printf("London to Paris is: %.2f km as the bird flyes\n", earth.distance(london, paris))
+
+	//Find the distance from your city to the capital of your country.
+	// Cary is 35.7915° N, 78.7811° W
+	// Washington, D.C./Coordinates 38.9072° N, 77.0369° W
+	cary := location{Name: "Cary, NC", Lat: coordinate{d: 35.7915, h: 'N'}, Long: coordinate{d: 78.7811, h: 'W'}}
+	washDC := location{Name: "Washington DC", Lat: coordinate{d: 38.9072, h: 'N'}, Long: coordinate{d: 77.0369, h: 'W'}}
+	fmt.Printf("Distance between %v and %v is: %.2f km as the bird flyes", cary.Name, washDC.Name, earth.distance(cary, washDC))
 
 	fmt.Printf("\n\n########## END ############\n")
 }
-
-//To work with universe.go
-// //create a NewUniverse and Show it.
-// newUniverse := NewUniverse()
-// tempUniverse := NewUniverse()
-// //newUniverse.Show()
-// //Seed the Universe and Show it.
-// newUniverse.Seed()
-// newUniverse.Show()
-// //Clear the Screen on MAC
-// //fmt.Println("\033[H")
-// //Step the Universe and Show it.
-// Step(newUniverse, tempUniverse)
-// newUniverse = tempUniverse
-// //fmt.Printf("\033[H")
-// newUniverse.Show()
-
-// To work with Words.go
-// textArgument := "As far as eye could reach he saw nothing but the stems of the great plants about him receding in the violet shade, and far overhead
-// the multiple transparency of huge leaves filtering the sunshine to the solemn splendour of twilight in which he walked. Whenever he felt able he
-// ran again; the ground continued soft and springy, covered with the same resilient weed which was the first thing his hands had touched in Malacandra.
-// Once or twice a small red creature scuttled across his path, but otherwise there seemed to be no life stirring in the wood; nothing to fear—except
-// the fact of wandering unprovisioned and alone in a forest of unknown vegetation thousands or millions of miles beyond the reach or knowledge of man."
-// words(textArgument)
