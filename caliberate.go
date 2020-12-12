c593b66b4caeb0d215413d8232fc14811fa8fb30
Rob Murgai <smurgai@gmail.com>
2020-12-12 08:08:20 -0500

    Update exercise infomration for reuse

--- caliberate.go
+++ caliberate.go
@@ -1,3 +1,41 @@
+// First Class Functions: In Go you can assign functions to variables, pass functions to functions, and write functions that return
+// functions. This ability is defined as Functions being first-class, as in they work in all the places that integers, strings, and other
+// types work.
+// Given the following code:
+// 		package main
+//
+// 		import "fmt"
+//
+// 		type kelvin float64
+//
+// 		// sensor function type
+// 		type sensor func() kelvin
+//
+// 		func realSensor() kelvin {
+//     		return 0
+// 		}
+//
+// 		func calibrate(s sensor, offset kelvin) sensor {
+//     		return func() kelvin {
+//          		return s() + offset
+//     		}
+// 		}
+//
+// 		func main() {
+//     		sensor := calibrate(realSensor, 5)
+//     		fmt.Println(sensor())
+// 		}
+//
+// Exercise 1) Rather than passing 5 as an argument to calibrate, declare and pass a variable. Modify the variable and you’ll notice that
+// calls to sensor() still result in 5. That’s because the offset parameter is a copy of the argument (pass by value).
+//
+// Exercise 2) Use calibrate with the following fakeSensor function to create a new sensor function. Call the new sensor function multiple
+// times and notice that the original fakeSensor is still being called each time, resulting in random values.
+//
+// 		func fakeSensor() kelvin {
+//     		return kelvin(rand.Intn(151) + 150)
+// 		}
+
 package main
 
 import (
@@ -6,7 +44,7 @@ import (
 	"time"
 )
 
-//  Use this in main.go as the funtion to understand what calliberate is doing
+//  Use this in main.go as the function to understand what calliberate is doing
 //  func main() {
 // 	fmt.Printf("######### START ###########\n\n")
 
