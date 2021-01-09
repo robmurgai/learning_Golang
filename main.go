ff501ffdab82f68dc41117e65a4cb5c7215b61fd
Rob Murgai <smurgai@gmail.com>
2021-01-09 06:42:50 -0500

    main.go reset
    Signed-off-by: Rob Murgai <smurgai@gmail.com>

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"fmt"
-
-	gophercises "./gophercises"
 )
 
 func main() {
 
 	fmt.Printf("########## Starting Exercise ##########\n\n")
-
-	gophercises.Quiz()
 
 	fmt.Printf("\n########## Ending Exercise ##########\n")
 
