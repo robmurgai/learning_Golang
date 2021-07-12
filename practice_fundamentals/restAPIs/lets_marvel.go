e13a908c69557cf20761e8914f3158852fdd8b90
Rob Murgai <smurgai@gmail.com>
2021-07-12 07:55:51 -0400

    Understand how to consume Rest APIs with Go
    - Moved all Rest API related work in practice_fundamentals/restAPIs folder
    - rest_api.go is like a libraries file with the common Get, Put, functions.  Currently
    using this as reference but evenutally wil turn it into a methods file so eventualy
    I call the methods in that file everytime I want to use Rest APIs.
    - Also created a lets_pokemon.go and lets_marvel.go to play with those respoective
    APIs.  Pokemon APIs do not require Authentication, Marvel APIs do.
    - Using config.yaml to store API Keys.
    
    Signed-off-by: Rob Murgai <smurgai@gmail.com>

--- practice_fundamentals/restAPIs/lets_marvel.go
+++ practice_fundamentals/restAPIs/lets_marvel.go
@@ -1,7 +1,41 @@
 package practicefundamentals
 
+// Expect the Response to look something like this.
+// {
+// "code": 200,
+// "status": "Ok",
+// "copyright": "© 2021 MARVEL",
+// "attributionText": "Data provided by Marvel. © 2021 MARVEL",
+// "attributionHTML": "<a href=\"http://marvel.com\">Data provided by Marvel. © 2021 MARVEL</a>",
+// "etag": "66e10fa20cdf7ef9db54b4dc9ddc1dad71601e44",
+// "data": {
+//   "offset": 0,
+//   "limit": 20,
+//   "total": 1493,
+//   "count": 20,
+//   "results": [
+// 	{
+//    "id": 1017100,
+//    "name": "A-Bomb (HAS)",
+//    "description": "Rick Jones has been Hulk's best bud since day one, but now he's more than a friend...he's a teammate! Transformed by a Gamma energy explosion,
+//    A-Bomb's thick, armored skin is just as strong and powerful as it is blue.",
+//    "modified": "2013-09-18T15:54:04-0400",
+//    "thumbnail": {
+//      "path": "http://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16",
+//      "extension": "jpg"
+//    },
+//    "resourceURI": "http://gateway.marvel.com/v1/public/characters/1017100",
+//    "comics": {
+//      "available": 3,
+//      "collectionURI": "http://gateway.marvel.com/v1/public/characters/1017100/comics",
+//      "items": [
+//      {
+// 	       "resourceURI": "http://gateway.marvel.com/v1/public/comics/40632",
+// 	       "name": "Hulk (2008) #53"
+//        },
+//      {
+
 import (
-	"bytes"
 	"crypto/md5"
 	"encoding/json"
 	"fmt"
@@ -11,9 +45,40 @@ import (
 	"time"
 )
 
+// Store Response in an appropriate Struct
+type items struct {
+	Name string `json:"name"`
+}
+
+type comics struct {
+	Available int     `json:"available"`
+	Items     []items `json:"items"`
+}
+
+type results struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Comics      comics `json:"comics"`
+}
+
+type marvelData struct {
+	Offset  int       `json:"offset"`
+	Limit   int       `json:"limit"`
+	Total   int       `json:"total"`
+	Count   int       `json:"count"`
+	Results []results `json:"results"`
+}
+
+type marvelCharecters struct {
+	Code   int        `json:"code"`
+	Status string     `json:"status"`
+	Etag   string     `json:"etag"`
+	Data   marvelData `json:"data"`
+}
+
 func LetsMarvel() {
 
-	// Set the API EnPoint URL
+	// Set the API EndPoint URL
 	apiDomain := "marvel"
 	apiEndPoint := "https://gateway.marvel.com/v1/public/characters"
 
@@ -43,104 +108,83 @@ func LetsMarvel() {
 
 	fmt.Printf("API Enpoint: %v\n", apiEndPoint)
 
-	// Make a REST API Get Call
-
-	response, err := http.Get(apiEndPoint)
-	if err != nil {
-		fmt.Printf("Error http.Get(%v): %v\n", apiEndPoint, err.Error())
-		panic(err)
-	}
-
-	defer response.Body.Close()
-
-	responseInBytes, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Printf("Error trying to read Response: %v\n", err.Error())
-		panic(err)
-	}
-
-	// fmt.Println(string(responseInBytes))
-
-	// Preety Print the JSON response.
-
-	var prettyResponse bytes.Buffer
-	err = json.Indent(&prettyResponse, responseInBytes, "", "  ")
-	if err != nil {
-		fmt.Printf("Error: json.Indent(%v): %v", responseInBytes, err.Error())
-		panic(err)
-	}
-
-	fmt.Printf("%s", prettyResponse)
-
-	// Create a struct for the Response.
-	// {
-	// "code": 200,
-	// "status": "Ok",
-	// "copyright": "© 2021 MARVEL",
-	// "attributionText": "Data provided by Marvel. © 2021 MARVEL",
-	// "attributionHTML": "<a href=\"http://marvel.com\">Data provided by Marvel. © 2021 MARVEL</a>",
-	// "etag": "66e10fa20cdf7ef9db54b4dc9ddc1dad71601e44",
-	// "data": {
-	//   "offset": 0,
-	//   "limit": 20,
-	//   "total": 1493,
-	//   "count": 20,
-	//   "results": [
-	// 	{
-	//    "id": 1017100,
-	//    "name": "A-Bomb (HAS)",
-	//    "description": "Rick Jones has been Hulk's best bud since day one, but now he's more than a friend...he's a teammate! Transformed by a Gamma energy explosion, A-Bomb's thick, armored skin is just as strong and powerful as it is blue. And when he curls into action, he uses it like a giant bowling ball of destruction! ",
-	//    "modified": "2013-09-18T15:54:04-0400",
-	//    "thumbnail": {
-	//      "path": "http://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16",
-	//      "extension": "jpg"
-	//    },
-	//    "resourceURI": "http://gateway.marvel.com/v1/public/characters/1017100",
-	//    "comics": {
-	//      "available": 3,
-	//      "collectionURI": "http://gateway.marvel.com/v1/public/characters/1017100/comics",
-	//      "items": [
-	//      {
-	// 	       "resourceURI": "http://gateway.marvel.com/v1/public/comics/40632",
-	// 	       "name": "Hulk (2008) #53"
-	//        },
-	//      {
-
-	type items struct {
-		Name string `json:"name"`
-	}
-
-	type comics struct {
-		Available int     `json:"available"`
-		Items     []items `json:"items"`
-	}
-
-	type results struct {
-		Name        string `json:"name"`
-		Description string `json:"description"`
-		Comics      comics `json:"comics"`
-	}
-
-	type marvelData struct {
-		Total   int       `json:"toal"`
-		Count   int       `json:"count"`
-		Results []results `json:"results`
-	}
-
-	type marvelCharecters struct {
-		Code   int        `json:"code"`
-		Status string     `json:"status"`
-		Etag   string     `json:"etag"`
-		Data   marvelData `json:"data"`
-	}
-
 	var mc marvelCharecters
 
-	err = json.Unmarshal(responseInBytes, &mc)
-	if err != nil {
-		fmt.Printf("Error: json.Unmarshal(response): %v", err.Error())
-		panic(err)
+	//Get the names of all 1493 charectors.
+	var charectors []string
+
+	// Make multiple API calls till you have all the results.
+	// Start with your offset set to 0, and increment the offset by the total results received in each API response, ie limit which is 100 as per the API docs.
+	// Increment the offset by this limit, and make another API call.
+	// Keep making calls till you have all the results, which will be when the offset is greater than the total results.
+
+	limit := 100
+	total := 0
+
+	for offset := 0; offset <= total; offset += limit {
+
+		// Add offset and limit to the API End Point
+		fmt.Printf("limit: %v\n", limit)
+		fmt.Printf("offset: %v\n", offset)
+		updatedApiEndPoint := apiEndPoint + "&limit=" + fmt.Sprint(limit) + "&offset=" + fmt.Sprint(offset)
+		fmt.Printf("API Enpoint Updated: %v\n", updatedApiEndPoint)
+		response, err := http.Get(updatedApiEndPoint)
+		if err != nil {
+			fmt.Printf("Error http.Get(%v): %v\n", updatedApiEndPoint, err.Error())
+			panic(err)
+		}
+
+		defer response.Body.Close()
+
+		responseInBytes, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Printf("Error trying to read Response: %v\n", err.Error())
+			panic(err)
+		}
+		// fmt.Println(string(responseInBytes))
+
+		// Preety Print the JSON response.
+
+		// var prettyResponse bytes.Buffer
+		// err = json.Indent(&prettyResponse, responseInBytes, "", "  ")
+		// if err != nil {
+		// 	fmt.Printf("Error: json.Indent(%v): %v", responseInBytes, err.Error())
+		// 	panic(err)
+		// }
+		// fmt.Printf("%s", prettyResponse)
+
+		// for each API call, get all the charectors
+		err = json.Unmarshal(responseInBytes, &mc)
+		if err != nil {
+			fmt.Printf("Error: json.Unmarshal(response): %v", err.Error())
+			panic(err)
+		}
+
+		// fmt.Printf("mc: %+v\n", mc)
+
+		fmt.Printf("Received Response from Marvel\n")
+		fmt.Printf("Code: %v\n", mc.Code)
+		fmt.Printf("Status: %v\n", mc.Status)
+		fmt.Printf("Etag: %v\n", mc.Etag)
+		fmt.Printf("Marvel said they have %v Charectors in thier Comics\n", mc.Data.Total)
+		fmt.Printf("They sent information on %v\n", mc.Data.Count)
+
+		//Update total.
+		total = mc.Data.Total
+
+		// Store charectors in the resonse to our "charectors" array
+		for _, value := range mc.Data.Results {
+			charectors = append(charectors, value.Name)
+			// fmt.Printf("  \n%v", value.Name)
+		}
+
+		fmt.Printf("Stored %v Charetors for reference\n\n", len(charectors))
+
 	}
 
-	fmt.Printf("mc: %+v\n", mc)
+	fmt.Printf("%v charectors stored\n", len(charectors))
+	for _, value := range charectors {
+		fmt.Printf("%v, ", value)
+	}
+
 }
